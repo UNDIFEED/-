c811091f6a08da886903d9dc46306f3d2262f3b1
Sparkererer <154579860+Sparkererer@users.noreply.github.com>
2023-12-28 21:06:32 +0800

    Delete code directory

--- code/flash.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FLASH_h
-#define __FLASH_h
-
-void save_data(int sector_num,int page_num,int save_num);
-void read_data(int sector_num,int page_num,int save_num);
-
-#endif
-
-
--- code/upcomputer.h
+++
@@ -1,6 +0,0 @@
-#ifndef _upcomputer_h_
-#define _upcomputer_h_
-
-void Upcomputer_Send(float data1, float data2, float data3, float data4);
-
-#endif
